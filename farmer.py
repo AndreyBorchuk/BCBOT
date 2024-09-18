e217f9f160f0d4b81a7da992ddf85f5b5f811af1
AndreyBorchuk <79987999+AndreyBorchuk@users.noreply.github.com>
2024-09-18 13:59:19 +0300

    Add files via upload

--- farmer.py
+++ farmer.py
@@ -11,6 +11,8 @@ top_drop_accounts = open("top_drop_accounts.json")
 top_drop_data = json.load(top_drop_accounts)
 top_drop_accounts.close()
 
+top_mobs = дописать
+
 while True:
     for i in data.keys():
         try:
@@ -20,9 +22,12 @@ while True:
                 while(count != 0):
                     api.request_room(i, "playcamp", "playcamp")
                     count = api.daily_claim(i)
-                    if (count is None or count == 0):
+                    if (count is None):
                         print("Error while daily claim")
                         count = 0
+                        continue
+                    if (count == 0):
+                        print("No free energy now")
                         continue
                     battle_id = api.decr_energy(i, count)
                     print(count, end=" ")
@@ -38,6 +43,21 @@ while True:
                     accounts = open("accounts.json", "w")
                     json.dump(data, accounts, indent=4)
                     accounts.close()
+                monster_inventory = api.inventory(i)
+                if (monster_inventory is None):
+                    print("Error while getting monster inventory")
+                    continue
+                teams = monster_inventory["monster_inventory"]["teams"]
+                to_sell = [[]]
+                for monster in monster_inventory["monster_inventory"]["monsters"]:
+                    if (monster["id"] in top_mobs):
+                        continue
+                    if (len(to_sell[-1]) >= 25):
+                        to_sell.append([monster["inventory_id"]])
+                    else:
+                        to_sell[-1].append(monster["inventory_id"])
+                for targets in to_sell:
+                    api.monster_sell(i, targets)
                 if (data[i][1] + 86400 <= time.time()):
                     status = api.character_updating("trainer_male_6", i, "templegatecamp")
                     if (status is None):
